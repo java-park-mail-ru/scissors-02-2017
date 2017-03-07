3d95ac71fe8b77563497c5408044c01d346be5c8
imabu <mfumfu17@gmail.com>
2017-03-07 21:47:58 +0300

    изменение класса ошибок

--- src/main/java/game/UserController.java
+++ src/main/java/game/UserController.java
@@ -26,20 +26,21 @@ public class UserController {
         final String password = body.getPassword();
 
         if (StringUtils.isEmpty(login)) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Error(Error.EMPTY_LOGIN));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResponseError.EMPTY_LOGIN);
         }
         if (StringUtils.isEmpty(password)) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Error(Error.EMPTY_PASSWORD));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResponseError.EMPTY_PASSWORD);
         }
         if (StringUtils.isEmpty(email)) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Error(Error.EMPTY_EMAIL));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResponseError.EMPTY_EMAIL);
         }
-        final UserProfile newUser = accountService.putIfAbsent(body);
+
+        final UserProfile newUser = accountService.addUser(body);
         if (newUser != null) {
             httpSession.setAttribute(KEY, login);
             return ResponseEntity.status(HttpStatus.OK).body(newUser.toInfo());
         } else {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Error(Error.ERROR_LOGIN));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResponseError.ERROR_LOGIN);
         }
     }
 
@@ -48,10 +49,10 @@ public class UserController {
         final String login = body.getLogin();
         final String password = body.getPassword();
         if (StringUtils.isEmpty(login)) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Error(Error.EMPTY_LOGIN));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResponseError.EMPTY_LOGIN);
         }
         if (StringUtils.isEmpty(password)) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Error(Error.EMPTY_PASSWORD));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResponseError.EMPTY_PASSWORD);
         }
         final UserProfile user = accountService.getUser(login);
         if (user != null) {
@@ -60,7 +61,7 @@ public class UserController {
                 return ResponseEntity.status(HttpStatus.OK).body(user.toInfo());
             }
         }
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Error(Error.ERROR_SIGNUP));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResponseError.ERROR_SIGNUP);
     }
 
     @DeleteMapping(path = "/api/session")
@@ -69,14 +70,14 @@ public class UserController {
             httpSession.setAttribute(KEY, null);
             return ResponseEntity.status(HttpStatus.OK).body(null);
         } else {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Error(Error.ERROR_SESSION));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResponseError.ERROR_SESSION);
         }
     }
 
     @GetMapping(path = "/api/session")
     public ResponseEntity<?> userOfCurrentSession(HttpSession httpSession) {
         if (httpSession.getAttribute(KEY) == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Error(Error.ERROR_AUTH));
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResponseError.ERROR_AUTH);
         }
         final String login = (String) httpSession.getAttribute(KEY);
         final UserProfile user = accountService.getUser(login);
@@ -86,11 +87,11 @@ public class UserController {
     @GetMapping(path = "/api/user/{login}")
     public ResponseEntity<?> getUserData(@PathVariable(value = "login") String login, HttpSession httpSession) {
         if (httpSession.getAttribute(KEY) == null) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new Error(Error.ERROR_AUTH));
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ResponseError.ERROR_AUTH);
         }
         final UserProfile user = accountService.getUser(login);
         if (user == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new Error(Error.ERROR_USER));
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ResponseError.ERROR_USER);
         }
         return ResponseEntity.status(HttpStatus.OK).body(user.toInfo());
     }
@@ -99,7 +100,7 @@ public class UserController {
     public ResponseEntity<?> changePassword(@PathVariable(value = "login") String login, @RequestBody UserAuth body, HttpSession httpSession) {
         final String attrib = (String) httpSession.getAttribute(KEY);
         if (attrib == null || attrib != login) {
-            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new Error(Error.ERROR_AUTH));
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(ResponseError.ERROR_AUTH);
         }
         accountService.changePassword(login, body);
         return ResponseEntity.status(HttpStatus.OK).body(null);
